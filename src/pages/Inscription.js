8f9f087c3eea3ec09d9b197d4484e3802a3aee8e
Adrien Ducourthial <adrien.popo60@gmail.com>
2020-12-03 20:36:25 +0100

    path inscription

---
+++ src/pages/Inscription.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/header'
+import Form from '../components/form'
+
+
+export default function Inscription(props) {
+
+return (
+    <Form>
+        <input id="login" type="text"></input>
+    </Form>
+)
+
+}
