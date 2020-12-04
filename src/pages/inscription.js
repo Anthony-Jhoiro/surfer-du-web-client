f23f81c7334fb805e1a12bd82eb8c4757bd318ca
Anthony <anthony.quere@lacatholille.fr>
2020-12-04 03:32:59 +0100

    Feat : Add make request

--- src/pages/inscription.js
+++ src/pages/inscription.js
@@ -9,28 +9,16 @@ import '../styles/pages/inscription.css'
 // composants
 import Header from "../components/header"
 import Form from '../components/form'
+import { TextField } from '@material-ui/core';
 
 export default function Inscription(props) {
 
     return (
         <>
             <Header/>
-            <Form>
-                <Grid container direction="column" justify="center" alignItems="center" className="view">
-                    <Grid>
-                        <label> Nom d'utilisateur :</label>
-                        <input id="pseudo" type="text" placeholder="Ecris ton nom meme"></input>
-                    </Grid>
-                    <Grid>
-                        <label> Adresse E-mail : </label>
-                        <input id="mail" type="email" placeholder="Met ton mail"></input>
-                    </Grid>
-                    <Grid>
-                        <label> Mot de passe : </label>
-                        <input id="mdp" type="password" placeholder="Ton mot de passe"></input>
-                    </Grid>
-                </Grid>
-            </Form>
+            <form autoComplete="off">
+                <TextField id="i-username" label="Username" />
+            </form>
         </>
     )
 
